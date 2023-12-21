4fdab7333f20aea84758dffe304ed1b87bd071d2
Robin Six <55784799+sixrobin@users.noreply.github.com>
2023-12-22 00:04:01 +0100

    Constants + errors count print

--- unity_files_scan.py
+++ unity_files_scan.py
@@ -2,6 +2,11 @@ import os
 from termcolor import colored
 
 
+WHITE, RED, GREEN, YELLOW, CYAN, BLUE = 'white', 'red', 'green', 'yellow', 'cyan', 'blue'
+BOLD, UNDERLINE, REVERSE = 'bold', 'underline', 'reverse'
+UNDERSCORE = '_'
+
+
 def enable_terminal_coloring():
     os.system('color')
 
@@ -19,14 +24,20 @@ def ask_user_prefix():
     return input('Enter a prefix to check in asset names (leave blank not to check prefixes): ')
 
 
+def print_space_line():
+    print('------------------------------------------------------------')
+
+
 def print_invalid_files(text, files_list):
     if len(files_list) == 0:
-        return
+        return 0
 
-    print('\n------------------------------------------------------------')
-    print(colored(f'\n{text} ({len(files_list)} found):', 'white', attrs=['bold']))
+    print_space_line()
+    print(colored(f'\n{text} ({len(files_list)} found):', WHITE, attrs=[BOLD]))
     for invalid_file in files_list:
         print('- ' + invalid_file)
+
+    return len(files_list)
 
 
 def scan():
@@ -35,7 +46,7 @@ def scan():
 
     scan_src = os.getcwd()
     folders = get_folders(scan_src)
-    print(f'Scanning {scan_src}...')
+    print(f'Scanning {colored(scan_src, WHITE, attrs=[BOLD])}...')
 
     missing_prefix = []
     invalid_caps = []
@@ -50,10 +61,10 @@ def scan():
 
             # Prefix check.
             if required_prefix and not file_name.startswith(required_prefix):
-                missing_prefix.append(colored(required_prefix, 'red') + file_name)
+                missing_prefix.append(colored(required_prefix, RED) + file_name)
 
             # Capitals check.
-            words = file_name.split('_')
+            words = file_name.split(UNDERSCORE)
             for word in words:
                 if not word:  # Asset name first character is an underscore.
                     continue
@@ -61,13 +72,23 @@ def scan():
                     colored_file_name = ''
                     for i in range(len(words)):
                         w = words[i]
-                        w = w if not w[0].islower() else colored(w[0], 'red') + w[1:]
-                        colored_file_name += w + ('_' if i < len(words) - 1 else '')
+                        w = w if not w[0].islower() else colored(w[0], RED) + w[1:]
+                        colored_file_name += w + (UNDERSCORE if i < len(words) - 1 else '')
                     invalid_caps.append(colored_file_name)
                     break
 
-    print_invalid_files(f'Missing \"{required_prefix}\" prefix', missing_prefix)
-    print_invalid_files('Invalid capitals', invalid_caps)
+    errors_count = 0
+    errors_count += print_invalid_files(f'Missing \"{required_prefix}\" prefix', missing_prefix)
+    errors_count += print_invalid_files('Invalid capitals', invalid_caps)
+
+    print('\n')
+    print_space_line()
+    print('\n')
+
+    if errors_count == 0:
+        print(colored('No error found!', GREEN))
+    else:
+        print(colored(f'{str(errors_count)} error(s) found!', RED))
 
 
 if __name__ == "__main__":
