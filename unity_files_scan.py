b6d4d65012cf89cbebb9ff9edb10e1e221cb5dd8
Robin Six <55784799+sixrobin@users.noreply.github.com>
2023-12-21 22:40:22 +0100

    Scan all files recursively (only names print)

--- unity_files_scan.py
+++ unity_files_scan.py
@@ -2,8 +2,24 @@ import os
 from termcolor import colored
 
 
+def get_folders(scan_src):
+    folders = []
+    for r, d, f in os.walk(scan_src):
+        for folder in d:
+            folders.append(os.path.join(r, folder))
+    return folders
+
+
 def scan():
-    print(colored('Scanning...', 'green'))
+    scan_src = os.getcwd()
+    folders = get_folders(scan_src)
+    print(f'Scanning {scan_src}...')
+
+    for folder in folders:
+        os.chdir(folder)
+        for file in os.listdir():
+            if not file.endswith('.meta'):
+                print(file)
 
 
 if __name__ == "__main__":
