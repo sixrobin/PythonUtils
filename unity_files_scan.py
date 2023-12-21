93ca79ff11531f5175de2bf3ccbc7f85faad9558
Robin Six <55784799+sixrobin@users.noreply.github.com>
2023-12-21 23:11:22 +0100

    Caps on words first chars check

--- unity_files_scan.py
+++ unity_files_scan.py
@@ -8,9 +8,10 @@ def enable_terminal_coloring():
 
 def get_folders(scan_src):
     folders = []
-    for r, d, f in os.walk(scan_src):
-        for folder in d:
-            folders.append(os.path.join(r, folder))
+    for root, dirs, _ in os.walk(scan_src):
+        for folder in dirs:
+            folders.append(os.path.join(root, folder))
+
     return folders
 
 
@@ -19,6 +20,8 @@ def scan():
     folders = get_folders(scan_src)
     print(f'Scanning {scan_src}...')
 
+    invalid_caps = []
+
     for folder in folders:
         os.chdir(folder)
         for file in os.listdir():
@@ -26,7 +29,24 @@ def scan():
                 continue
 
             file_name = file.split('.')[0]
-            print(file_name)
+
+            # Capitals check.
+            words = file_name.split('_')
+            for word in words:
+                if not word:  # Asset name first character is an underscore.
+                    continue
+                if word[0].islower() and word[0].isalpha():
+                    colored_file_name = ''
+                    for i in range(len(words)):
+                        w = words[i]
+                        w = w if not w[0].islower() else colored(w[0], 'red') + w[1:]
+                        colored_file_name += w + ('_' if i < len(words) - 1 else '')
+                    invalid_caps.append(colored_file_name)
+                    break
+
+    print(colored(f'\n{'Invalid capitals'} ({len(invalid_caps)} found):'))
+    for invalid_file in invalid_caps:
+        print('- ' + invalid_file)
 
 
 if __name__ == "__main__":
