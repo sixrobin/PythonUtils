235cdadc5aefbdf2cc9ad1864c117e9106b19a46
Robin Six <55784799+sixrobin@users.noreply.github.com>
2023-12-21 22:19:27 +0100

    Scan function setup

--- unity_files_scan.py
+++ unity_files_scan.py
@@ -1,1 +1,17 @@
-print('Hello World!')
+import os
+from termcolor import colored
+
+
+def scan():
+    print(colored('Scanning...', 'green'))
+
+
+if __name__ == "__main__":
+    os.system('color')  # Enable terminal coloring.
+
+    try:
+        scan()
+    except Exception as e:
+        print(e)
+
+    input()
